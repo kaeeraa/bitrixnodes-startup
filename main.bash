338b09a9b5461b54de94c4b2acc1e2d8decd7813
Kaeeraa <kaeeraa@nebula-nook.ru>
2024-06-01 14:00:48 +0300

    Update main.bash

--- main.bash
+++ main.bash
@@ -9,6 +9,19 @@ fi
 if [[ ! -f cache/mojang_$VERSION.jar ]]
 then
   echo "Vanilla.jar Не найден!"
-  curl -m 45 -L -O http://91.239.148.132:1111/get/$VERSION
-  mv mojang_$VERSION.jar cache/mojang_$VERSION.jar
+  curl -o cache/mojang-$VERSION.jar "http://91.239.148.132:1111/get?v=$VERSION"
 fi
+
+if [[ -f eula.txt ]]
+then
+  if [[ grep -q eula=false eula.txt ]]
+  then
+    rm eula.txt
+    touch eula.txt
+    echo "# Не изменяйте данный файл, если хотите что бы ваш сервер работал!" > eula.txt
+    echo "eula=true" > eula.txt
+  fi
+fi
+
+
+
