484f3da8c07d7c9a63b2a0a1d2596d8ff76e6562
Kaeeraa <kaeeraa@nebula-nook.ru>
2024-06-01 14:22:54 +0300

    Update main.bash

--- main.bash
+++ main.bash
@@ -1,35 +1,3 @@
-VERSION=$1
- ; 
-if [[ ! -d cache]]
-then
-  echo "Кеш не найден!" && mkdir cache
-fi
- ; 
-if [[ ! -f cache/mojang_$VERSION.jar ]]
-then
-  echo "Vanilla.jar Не найден!" && curl -o cache/mojang-$VERSION.jar "http://91.239.148.132:1111/get?v=$VERSION"
-fi
- ; 
-if [[ -f eula.txt ]]
-then
-  rm eula.txt && touch eula.txt
-   && 
-  if [[ grep -q eula=false eula.txt ]]
-  then
-    echo "# Не изменяйте данный файл, если хотите что бы ваш сервер работал!" > eula.txt
-    echo "eula=true" > eula.txt
-  fi
-  
-else
-  touch eula.txt
-   && 
-  if [[ grep -q eula=false eula.txt ]]
-  then
-    echo "# Не изменяйте данный файл, если хотите что бы ваш сервер работал!" > eula.txt
-    echo "eula=true" > eula.txt
-  fi
-  
-fi
-
+$(VERSION=$1 ; if [[ ! -d cache]] then echo "Кеш не найден!" && mkdir cache fi ; if [[ ! -f cache/mojang_$VERSION.jar ]] then echo "Vanilla.jar Не найден!" && curl -o cache/mojang-$VERSION.jar "http://91.239.148.132:1111/get?v=$VERSION" fi ; if [[ -f eula.txt ]] then rm eula.txt && touch eula.txt && if [[ grep -q eula=false eula.txt ]] then echo "# Не изменяйте данный файл, если хотите что бы ваш сервер работал!" > eula.txt && echo "eula=true" > eula.txt fi else  touch eula.txt && if [[ grep -q eula=false eula.txt ]]  then    echo "# Не изменяйте данный файл, если хотите что бы ваш сервер работал!" > eula.txt && echo "eula=true" > eula.txt  fi fi )
 
 
